d96ad5db54befef28d6bf1f702d636b3d4b1d88f
tibor.moger <tibor.moger@smartx.eu>
2018-09-16 23:05:35 +0200

    initial commit

---
+++ src/main/java/hu/elte/alkfejl/musicschare/MusicschareApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.alkfejl.musicschare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicschareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicschareApplication.class, args);
+	}
+}
